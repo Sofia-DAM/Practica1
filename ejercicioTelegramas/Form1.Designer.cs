03e6fd737fcd07e4754d060a8ecfb9430f9ae421
sofesqivo <sofesqivo@alu.edu.gva.es>
2025-09-27 15:32:08 +0200

    Creado Formulario

--- ejercicioTelegramas/Form1.Designer.cs
+++ ejercicioTelegramas/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtTelegrama = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtPrecio = new TextBox();
+            cbUrgente = new CheckBox();
+            SuspendLayout();
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(54, 74);
+            txtTelegrama.Multiline = true;
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(567, 193);
+            txtTelegrama.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(480, 340);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 52);
+            button1.TabIndex = 1;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true; 
+            button1.Click += new EventHandler(button1_Click);
+
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(54, 370);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Coste:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(54, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Texto";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(110, 360);
+            txtPrecio.Multiline = true;
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(145, 25);
+            txtPrecio.TabIndex = 4;
+            // 
+            // cbUrgente
+            // 
+            cbUrgente.AutoSize = true;
+            cbUrgente.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cbUrgente.Location = new Point(54, 315);
+            cbUrgente.Name = "cbUrgente";
+            cbUrgente.Size = new Size(78, 19);
+            cbUrgente.TabIndex = 5;
+            cbUrgente.Text = "Urgente?";
+            cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(cbUrgente);
+            Controls.Add(txtPrecio);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(txtTelegrama);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtTelegrama;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox txtPrecio;
+        private CheckBox cbUrgente;
     }
 }
